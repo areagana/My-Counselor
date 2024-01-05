2011f508400a6e6a7afd77b8bdf7afd1e328310a
areagana <areagana84@gmail.com>
2024-01-05 08:56:26 +0300

    addingautomatic backup bat

--- mysql_system_backup.bat
+++ mysql_system_backup.bat
@@ -1,4 +1,3 @@
 cd "C:\xampp\mysql\bin"
-mysqldump -hlocalhost -uroot -proot wits_college_database>"D:\SYSTEM BACKUPS\Wits_college_database_backups_%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.sql"
+./mysqldump.exe -hlocalhost -uroot -p counseling>"C:\Counselor_backup_%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.sql"
 
-//
